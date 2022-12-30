129af0e28023dda093d1cfbd11b57917c4b74172
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-12-30 22:32:55 +0200

    Fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,162 +1,23 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { toast } from 'react-toastify';
-import { Gallery } from './ImageGallery.styled';
-import Box from 'components/Box/Box';
-import fetchData from 'services/api';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-import Loader from 'components/Loader/Loader';
-import Button from 'components/Button/Button';
-import Modal from 'components/Modal/Modal';
+import { Gallery } from './ImageGallery.styled';
 
-const PER_PAGE = 20;
-
-export default class ImageGallery extends Component {
-  state = {
-    imgList: [],
-    page: 1,
-    totalPages: 0,
-    error: null,
-    status: 'idle',
-    isModalShown: false,
-    url: '',
-    tag: '',
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const { page } = this.state;
-    const { searchValue } = this.props;
-
-    if (prevProps.searchValue !== searchValue) {
-      this.setState({
-        imgList: [],
-        page: 1,
-        totalPages: 0,
-        status: 'pending',
-      });
-
-      fetchData(searchValue, page, PER_PAGE)
-        .then(res => {
-          if (res.hits.length < 1) {
-            toast.info('Nothing was found according to your request');
-            this.setState({
-              status: 'resolved',
-            });
-          }
-          this.setState({
-            imgList: res.hits,
-            totalPages: Math.ceil(res.totalHits / PER_PAGE),
-            status: 'resolved',
-          });
-          window.scrollTo({
-            top: 0,
-          });
-        })
-        .catch(error =>
-          this.setState({ error: error.response.data, status: 'rejected' })
-        );
-    }
-
-    if (
-      prevState.page !== page &&
-      prevProps.searchValue === searchValue &&
-      page > 1
-    ) {
-      this.setState({
-        status: 'pending',
-      });
-
-      fetchData(searchValue, page, PER_PAGE)
-        .then(res => {
-          this.setState({
-            imgList: [...prevState.imgList, ...res.hits],
-            status: 'resolved',
-          });
-          window.scrollBy({
-            top: 576,
-            behavior: 'smooth',
-          });
-        })
-        .catch(error =>
-          this.setState({ error: error.response.data, status: 'rejected' })
-        );
-    }
-  }
-
-  getNextPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  openModal = e => {
-    this.setState(state => ({
-      isModalShown: true,
-      url: e.target.dataset.url,
-      tag: e.target.alt,
-      status: 'pending',
-    }));
-  };
-
-  closeModal = () => {
-    this.setState(state => ({
-      isModalShown: false,
-    }));
-  };
-
-  closeLoader = () => {
-    this.setState(state => ({
-      status: 'idle',
-    }));
-  };
-
-  render() {
-    const { imgList, status, totalPages, page, url, tag } = this.state;
-
-    if (status === 'rejected') {
-      return (
-        <Box
-          width="100%"
-          height="100vh"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          {toast.error(this.state.error)}
-          <h1>Something went wrong, please reload this page</h1>
-        </Box>
-      );
-    }
-
-    return (
-      <>
-        <Gallery onClick={this.openModal}>
-          {imgList.map(({ webformatURL, largeImageURL, tags, id }) => (
-            <ImageGalleryItem
-              key={id}
-              webformatURL={webformatURL}
-              largeImageURL={largeImageURL}
-              tags={tags}
-            />
-          ))}
-        </Gallery>
-        {totalPages > 1 && totalPages > page && (
-          <Button handleClick={this.getNextPage} />
-        )}
-        {status === 'pending' && <Loader />}
-        {this.state.isModalShown && (
-          <Modal
-            onClose={this.closeModal}
-            onCloseLoader={this.closeLoader}
-            url={url}
-            tag={tag}
-          />
-        )}
-      </>
-    );
-  }
+export default function ImageGallery({ imgList, onClick }) {
+  return (
+    <Gallery onClick={onClick}>
+      {imgList.map(({ webformatURL, largeImageURL, tags, id }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          tags={tags}
+        />
+      ))}
+    </Gallery>
+  );
 }
 
 ImageGallery.propTypes = {
-  searchValue: PropTypes.string.isRequired,
+  imgList: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,7 +8,6 @@ const modalRoot = document.querySelector('#modal-root');
 export default class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
-    setTimeout(this.props.onCloseLoader, 250);
   }
 
   componentWillUnmount() {
@@ -43,4 +42,5 @@ export default class Modal extends Component {
 Modal.propTypes = {
   url: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
